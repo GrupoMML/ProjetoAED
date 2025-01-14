e67724c8238a3660b9b2e83068f9c4c845335695
Miguyy <miguelangelo123machado@gmail.com>
2025-01-14 15:18:09 +0000

    botão inicial + função de login

--- JUNTAR TUDO AQUI/AED_GRUPO02/projecto_app.py
+++ JUNTAR TUDO AQUI/AED_GRUPO02/projecto_app.py
@@ -38,75 +38,125 @@ app.configure(fg_color="black")
 app.iconbitmap("Images/1-f8c98aa8.ico")
 renderWindow(1280, 832, "GameON!")
 
-# Função para trocar entre os frames
+
+# ---------------FRAMES ---------------------
+#-----------------------------------------------------------------
 def showFrame(frame):
     """
     Oculta todos os frames e exibe apenas o especificado.
     """
-    initital_frame.pack_forget()
-    login_frame.pack_forget()
+    for widget in app.winfo_children():
+        widget.destroy()
     frame.pack(fill="both", expand=True)
+
+# ---------------FUNÇÃO LOGIN---------------------
+#-----------------------------------------------------------------
+def loginFunction():
+    # --------------DESIGN LOGIN ---------------------
+    #-----------------------------------------------------------------
+    login_frame = ctk.CTkFrame(app, width=800, height=500, fg_color="#2E2B2B", corner_radius=15)
+    login_frame.place(relx=0.5, rely=0.5, anchor="center")
+    imgIcon = ctk.CTkImage(Image.open("Images/Logo.png"), size=(290, 100))
+    imgIcon_label = ctk.CTkLabel(login_frame, image=imgIcon, text="", fg_color="#2E2B2B")
+    imgIcon_label.place(relx=0.5, rely=0.2, anchor="center")
+
+    input_frame = ctk.CTkFrame(login_frame, width=550, height=350, fg_color="#2E2B2B")
+    input_frame.place(relx=0.5, rely=0.6, anchor="center")  
+
+    username_entry = ctk.CTkEntry(input_frame, fg_color="white", width=290, height=37, border_color="#2E2B2B", corner_radius=5, 
+                                placeholder_text="USERNAME / EMAIL", placeholder_text_color="black", text_color="black",
+                                font=("Arial", 12, "bold"))
+    username_entry.pack(pady=5, anchor="center")
+
+    password_entry = ctk.CTkEntry(input_frame, fg_color="white", show="*", width=290, height=37, border_color="#2E2B2B", corner_radius=5, 
+                                placeholder_text="PASSWORD", placeholder_text_color="black", text_color="black",
+                                font=("Arial", 12, "bold"))
+    password_entry.pack(pady=5, anchor="center")
+
+    button_frame = ctk.CTkFrame(input_frame, fg_color="#2E2B2B")
+    button_frame.pack(pady=(20, 20), anchor="center")
+
+    loginBtn = ctk.CTkButton(button_frame, text="LOGIN", fg_color="#FFA500", hover_color="#FF5900", width=140, height=37, 
+                                border_color="#2E2B2B", text_color="black", font=ctk.CTkFont(size=20, weight="bold"), command="")
+    loginBtn.pack(side="left", padx=5, anchor="center")
+
+    signinBtn = ctk.CTkButton(button_frame, text="SIGN IN", fg_color="#FFA500", hover_color="#FF5900", width=140, height=37, 
+                                border_color="#2E2B2B", text_color="black", font=ctk.CTkFont(size=20, weight="bold"), command="")
+    signinBtn.pack(side="left", padx=5, anchor="center")
+
+    qrcode_frame = ctk.CTkFrame(input_frame, fg_color="#2E2B2B")
+    qrcode_frame.pack(pady=(5, 20), anchor="center")
+
+    qrcodeBtn = ctk.CTkButton(qrcode_frame, text="QR CODE", fg_color="#FFA500", hover_color="#FF5900", 
+                                width=292, height=37, border_color="#2E2B2B", text_color="black", 
+                                font=ctk.CTkFont(size=20, weight="bold"), command="")
+    qrcodeBtn.pack(padx=5, anchor="center")
+
+    # --------------FUNCIONALIDADES ---------------------
+    #-----------------------------------------------------------------
+    global currentUser
+    login_input = username_entry.get().strip()  
+    password = password_entry.get().strip() 
+
+    if login_input and password:
+        if '@' in login_input:
+            userFound = False
+            for file_name in os.listdir("users"):
+                if file_name.endswith(".txt"):
+                    userFile = os.path.join("users", file_name)
+                    file = open(userFile, "r")
+                    savedUsername = decodeBinary(file.readline().strip())
+                    savedPassword = decodeBinary(file.readline().strip())
+                    savedEmail = decodeBinary(file.readline().strip())
+                    file.close()
+
+                    if savedEmail == login_input: 
+                        if savedPassword == password:  
+                            currentUser = savedUsername
+                            userFound = True
+                        else:
+                            messagebox.showerror("Error", "Wrong password.")
+                            userFound = True
+                        break
+            if not userFound:
+                messagebox.showerror("Error", "Email not found.")
+        else:
+            userFile = os.path.join("users", f"{login_input}.txt")
+            if os.path.exists(userFile):
+                file = open(userFile, "r")
+                savedPassword = decodeBinary(file.readline().strip())
+                file.close()
+
+                if savedPassword == password:  
+                    currentUser = login_input
+                else:
+                    messagebox.showerror("Error", "Wrong password.")
+            else:
+                messagebox.showerror("Error", "User not found.")
+    else:
+        messagebox.showwarning("Warning", "Fill all the fields.")
 
 # --------------ECRÃ INICIAL ---------------------
 #-----------------------------------------------------------------
-initital_frame = ctk.CTkFrame(app, width=1280, height=832, fg_color="#2E2B2B", corner_radius=0)
-initital_frame.pack(fill="both", expand=True)
-
 imgIcon = ctk.CTkImage(Image.open("Images/Logo.png"), size=(450, 150))
-imgIcon_label = ctk.CTkLabel(initital_frame, image=imgIcon, text="")
+imgIcon_label = ctk.CTkLabel(app, image=imgIcon, text="")
 imgIcon_label.place(relx=0.5, rely=0.35, anchor="center")
 
-initial_msg_label = ctk.CTkLabel(initital_frame, text="WELCOME TO YOUR FAVORITE PLACE TO PLAY GAMES!",
+initial_msg_label = ctk.CTkLabel(app, text="WELCOME TO YOUR FAVORITE PLACE TO PLAY GAMES!",
                                   font=ctk.CTkFont(size=24, weight="bold"), text_color="white")
 initial_msg_label.place(relx=0.5, rely=0.5, anchor="center")
 
-button_frame = ctk.CTkFrame(initital_frame, width=480, height=85, fg_color="#202020", corner_radius=15)
+button_frame = ctk.CTkFrame(app, width=480, height=85, fg_color="#202020", corner_radius=15)
 button_frame.place(relx=0.5, rely=0.6, anchor="center")  
 
 initialBtn = ctk.CTkButton(button_frame, text="CLICK HERE IF YOU’RE ON!", text_color="black", fg_color="#FFA500",
                            font=ctk.CTkFont(size=20, weight="bold"), hover_color="#FF5900",
-                           command=lambda: showFrame(login_frame), width=480, height=85)
+                           command=lambda:loginFunction(), width=480, height=85)
 initialBtn.place(relx=0.5, rely=0.5, anchor="center") 
 
-# ---------------AREA UTILIZADOR (LOGIN) ---------------------
-#-----------------------------------------------------------------
-login_frame = ctk.CTkFrame(app, width=1280, height=832, fg_color="#2E2B2B", corner_radius=0)
 
-imgIcon = ctk.CTkImage(Image.open("Images/Logo.png"), size=(290, 100))
-imgIcon_label = ctk.CTkLabel(login_frame, image=imgIcon, text="", fg_color="#2E2B2B")
-imgIcon_label.place(relx=0.5, rely=0.2, anchor="center")
 
-input_frame = ctk.CTkFrame(login_frame, width=550, height=350, fg_color="#2E2B2B")
-input_frame.place(relx=0.5, rely=0.6, anchor="center")  
 
-username_entry = ctk.CTkEntry(input_frame, fg_color="white", width=290, height=37, border_color="#2E2B2B", corner_radius=5, 
-                               placeholder_text="USERNAME / EMAIL", placeholder_text_color="black", text_color="black",
-                               font=("Arial", 12, "bold"))
-username_entry.pack(pady=5, anchor="center")
-
-password_entry = ctk.CTkEntry(input_frame, fg_color="white", show="*", width=290, height=37, border_color="#2E2B2B", corner_radius=5, 
-                               placeholder_text="PASSWORD", placeholder_text_color="black", text_color="black",
-                               font=("Arial", 12, "bold"))
-password_entry.pack(pady=5, anchor="center")
-
-button_frame = ctk.CTkFrame(input_frame, fg_color="#2E2B2B")
-button_frame.pack(pady=(20, 20), anchor="center")
-
-loginBtn = ctk.CTkButton(button_frame, text="LOGIN", fg_color="#FFA500", hover_color="#FF5900", width=140, height=37, 
-                             border_color="#2E2B2B", text_color="black", font=ctk.CTkFont(size=20, weight="bold"), command="")
-loginBtn.pack(side="left", padx=5, anchor="center")
-
-signinBtn = ctk.CTkButton(button_frame, text="SIGN IN", fg_color="#FFA500", hover_color="#FF5900", width=140, height=37, 
-                              border_color="#2E2B2B", text_color="black", font=ctk.CTkFont(size=20, weight="bold"), command="")
-signinBtn.pack(side="left", padx=5, anchor="center")
-
-qrcode_frame = ctk.CTkFrame(input_frame, fg_color="#2E2B2B")
-qrcode_frame.pack(pady=(5, 20), anchor="center")
-
-qrcodeBtn = ctk.CTkButton(qrcode_frame, text="QR CODE", fg_color="#FFA500", hover_color="#FF5900", 
-                              width=292, height=37, border_color="#2E2B2B", text_color="black", 
-                              font=ctk.CTkFont(size=20, weight="bold"), command="")
-qrcodeBtn.pack(padx=5, anchor="center")
 
 # ---------------FIM DA FUNÇÃO DA APP ---------------------
 #-----------------------------------------------------------------
