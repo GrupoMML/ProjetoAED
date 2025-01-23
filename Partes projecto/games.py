bd61198b7da2e19cf3bbcc3c41014acd47ed7391
Manutex78 <manutex782006@gmail.com>
2025-01-23 16:04:48 +0000

    Organização

---
+++ Partes projecto/games.py
@@ -0,0 +1,151 @@
+#-----------------------------------------------------------------
+#Ficheiro com todas a funções relacionadas com os jogos
+# -----------------------------------------------------------------
+
+# ---------------BIBLIOTECAS ---------------------
+# -----------------------------------------------------------------
+
+import customtkinter as ctk
+from tkinter import messagebox
+import os
+import notifications as ntf
+import users
+
+# ---------------FUNCOES ---------------------
+# -----------------------------------------------------------------
+
+# ADMINISTRADOR:
+
+def addGame():
+    """
+    Função que adiciona um jogo à lista de jogos
+    """
+    nomeJogo = entryNomeJogo.get()
+    genero = entryGenero.get()
+    descricao = entryDescricao.get("1.0", "end-1c")
+    imagem = entryImagem.get()
+    if nomeJogo == "" or genero == "" or preco == "" or descricao == "" or dataLancamento == "" or classificacao == "" or plataforma == "" or imagem == "":
+        messagebox.showerror("Error", "Please fill in all fields!")
+    else:
+        #chamar função de notificações
+        ntf.addNotification(nomeJogo,genero)
+        file = open("jogos.txt", "a", encoding="utf-8")
+        file.write(f"{nomeJogo};{genero};{preco};{descricao};{dataLancamento};{classificacao};{plataforma};{imagem}\n")
+        file.close()
+        messagebox.showinfo("Success", "Game added successfully!")
+        entryNomeJogo.delete(0, "end")
+        entryGenero.delete(0, "end")
+        entryDescricao.delete("1.0", "end")
+        entryImagem.delete(0, "end")
+
+def deleteGame():
+    """
+    Função que apaga um jogo da lista de jogos
+    """
+    nomeJogo = entryNomeJogo.get()
+    if nomeJogo == "":
+        messagebox.showerror("Error", "Please fill in all fields!")
+    else:
+        ntf.deleteNotification(nomeJogo)
+        listaJogos = users.lerFicheiroJogos()
+        for linha in listaJogos:
+            jogo = linha.split(";")
+            if jogo[0] == nomeJogo:
+                listaJogos.remove(linha)
+                file = open("jogos.txt", "w", encoding="utf-8")
+                file.writelines(listaJogos)
+                file.close()
+                messagebox.showinfo("Success", "Game deleted successfully!")
+                entryNomeJogo.delete(0, "end")
+                break
+        else:
+            messagebox.showerror("Error", "Game not found!")
+
+def editGame():
+    """
+    Função que edita um jogo da lista de jogos
+    """
+    nomeJogo = entryNomeJogo.get()
+    genero = entryGenero.get()
+    preco = entryPreco.get()
+    descricao = entryDescricao.get("1.0", "end-1c")
+    dataLancamento = entryDataLancamento.get()
+    classificacao = entryClassificacao.get()
+    plataforma = entryPlataforma.get()
+    imagem = entryImagem.get()
+    if nomeJogo == "" or genero == "" or preco == "" or descricao == "" or dataLancamento == "" or classificacao == "" or plataforma == "" or imagem == "":
+        messagebox.showerror("Error", "Please fill in all fields!")
+    else:
+        listaJogos = users.lerFicheiroJogos()
+        for idx, linha in enumerate(listaJogos):
+            jogo = linha.split(";")
+            if jogo[0] == nomeJogo and jogo[1] == genero and jogo[2] == preco and jogo[3] == descricao and jogo[4] == dataLancamento and jogo[5] == classificacao and jogo[6] == plataforma and jogo[7] == imagem:
+                listaJogos[idx] = f"{nomeJogo};{genero};{preco};{descricao};{dataLancamento};{classificacao};{plataforma};{imagem}\n"
+                file = open("jogos.txt", "w", encoding="utf-8")
+                file.writelines(listaJogos)
+                file.close()
+                messagebox.showinfo("Success", "Game edited successfully!")
+                entryNomeJogo.delete(0, "end")
+                entryGenero.delete(0, "end")
+                entryPreco.delete(0, "end")
+                entryDescricao.delete("1.0", "end")
+                entryDataLancamento.delete(0, "end")
+                entryClassificacao.delete(0, "end")
+                entryPlataforma.delete(0, "end")
+                entryImagem.delete(0, "end")
+                break
+        else:
+            messagebox.showerror("Error", "Game not found!")
+
+# UTILIZADOR:
+
+def addGameFav(nomeJogo, currentUser,generoJogo):
+    """
+    Função que adiciona um jogo aos favoritos do utilizador
+    """
+    if nomeJogo == "":
+        messagebox.showerror("Error", "Please fill in all fields!")
+    else:
+        listaJogos = users.lerFicheiroJogos()
+        for linha in listaJogos:
+            jogo = linha.split(";")
+            if jogo[0] == nomeJogo:
+                file = open("jogosFavoritos.txt", "a", encoding="utf-8")
+                file.write(f"{currentUser};{nomeJogo};{generoJogo}\n")
+                file.close()
+                messagebox.showinfo("Success", "Game added to favorites successfully!")
+                break
+        else:
+            messagebox.showerror("Error", "Game not found!")
+
+def deleteGameFav(nomeJogo, currentUser):
+    """
+    Função que apaga um jogo dos favoritos do utilizador
+    """
+    if nomeJogo == "":
+        messagebox.showerror("Error", "Please fill in all fields!")
+    else:
+        listaJogosFav = users.lerFicheiroJogosFavoritos()
+        for linha in listaJogosFav:
+            jogo = linha.split(";")
+            if jogo[1] == nomeJogo and jogo[0] == currentUser:
+                listaJogosFav.remove(linha)
+                file = open("jogosFavoritos.txt", "w", encoding="utf-8")
+                file.writelines(listaJogosFav)
+                file.close()
+                messagebox.showinfo("Success", "Game deleted from favorites successfully!")
+                break
+        else:
+            messagebox.showerror("Error", "Game not found!")
+
+def addComentario(nomeJogo, currentUser, comentario):
+    """
+    Função que adiciona um comentário a um jogo
+    """
+    if nomeJogo == "" or comentario == "":
+        messagebox.showerror("Error", "Please fill in all fields!")
+    else:
+        file = open("comentarios.txt", "a", encoding="utf-8")
+        file.write(f"{nomeJogo};{currentUser};{comentario}\n")
+        file.close()
+        messagebox.showinfo("Success", "Comment added successfully!")
---
+++ Partes projecto/users.py
@@ -0,0 +1,162 @@
+#ficheiro python com as funções relacionadas com os utilizadores
+#estrutura da linha do ficheiro users.txt: username;email;password;nivelPermissao
+
+# ---------------BIBLIOTECAS ---------------------
+# -----------------------------------------------------------------
+
+import customtkinter as ctk
+from tkinter import messagebox
+import os
+import datetime
+
+# ---------------FUNCOES ---------------------
+# -----------------------------------------------------------------
+
+# ADMINISTRADOR:
+
+def addUser():
+    """
+    Função que adiciona um utilizador à lista de utilizadores
+    """
+    username = entryUsername.get()
+    email = entryEmail.get()
+    password = entryPassword.get()
+    nivelPerm = entryNivelPerm.get()
+    if username == "" or email == "" or password == "" or nivelPerm == "":
+        messagebox.showerror("Error", "Please fill in all fields!")
+    else:
+        listaUsers = lerFicheiroUsers()
+        for linha in listaUsers:
+            camposUser = linha.split(";")
+            if camposUser[0] == username:
+                messagebox.showerror("Error", "Username already exists!")
+                return
+            elif email.count("@") != 1 or email.count(".") == 0:
+                messagebox.showerror("Error", "Invalid email!")
+                return
+            elif camposUser[1] == email:
+                messagebox.showerror("Error", "Email already exists!")
+                return
+            elif len(password) < 8:
+                messagebox.showerror("Error", "Password must be at least 8 characters long!")
+                return
+            else:
+                file = open("users.txt", "a", encoding="utf-8")
+                file.write(f"{username};{email};{password};{nivelPerm}\n")
+                file.close()
+                messagebox.showinfo("Success", "User added successfully!")
+                entryUsername.delete(0, "end")
+                entryEmail.delete(0, "end")
+                entryPassword.delete(0, "end")
+                entryNivelPerm.delete(0, "end")
+                return
+
+def deleteUser():
+    """
+    Função que apaga um utilizador da lista de utilizadores
+    """
+    username = entryUsername.get()
+    if username == "":
+        messagebox.showerror("Error", "Please fill in all fields!")
+    else:
+        listaUsers = lerFicheiroUsers()
+        for linha in listaUsers:
+            user = linha.split(";")
+            if user[0] == username:
+                listaUsers.remove(linha)
+                file = open("users.txt", "w", encoding="utf-8")
+                file.writelines(listaUsers)
+                file.close()
+                messagebox.showinfo("Success", "User deleted successfully!")
+                entryUsername.delete(0, "end")
+                return
+        messagebox.showerror("Error", "User not found!")
+
+def editUser():
+    """
+    Função que edita um utilizador da lista de utilizadores
+    """
+    username = entryUsername.get()
+    email = entryEmail.get()
+    password = entryPassword.get()
+    nivelPerm = entryNivelPerm.get()
+    if username == "" or email == "" or password == "" or nivelPerm == "":
+        messagebox.showerror("Error", "Please fill in all fields!")
+    else:
+        listaUsers = lerFicheiroUsers()
+        for linha in listaUsers:
+            user = linha.split(";")
+            if user[0] == username:
+                listaUsers.remove(linha)
+                file = open("users.txt", "w", encoding="utf-8")
+                file.writelines(listaUsers)
+                file.close()
+                file = open("users.txt", "a", encoding="utf-8")
+                file.write(f"{username};{email};{password};{nivelPerm}\n")
+                file.close()
+                messagebox.showinfo("Success", "User edited successfully!")
+                entryUsername.delete(0, "end")
+                entryEmail.delete(0, "end")
+                entryPassword.delete(0, "end")
+                entryNivelPerm.delete(0, "end")
+                return
+        messagebox.showerror("Error", "User not found!")
+
+# UTILIZADOR:
+def signinFunction(newUsernameEntry,newEmailEntry,newPasswordEntry):
+    """
+    Função que regista um utilizador na lista de utilizadores
+    """
+    username = newUsernameEntry.get()
+    email = newEmailEntry.get()
+    password = newPasswordEntry.get()
+    if username == "" or email == "" or password == "":
+        messagebox.showerror("Error", "Please fill in all fields!")
+    else:
+        listaUsers = lerFicheiroUsers()
+        for linha in listaUsers:
+            camposUser = linha.split(";")
+            if camposUser[0] == username:
+                messagebox.showerror("Error", "Username already exists!")
+                return
+            elif email.count("@") != 1 or email.count(".") == 0:
+                messagebox.showerror("Error", "Invalid email!")
+                return
+            elif camposUser[1] == email:
+                messagebox.showerror("Error", "Email already exists!")
+                return
+            elif len(password) < 8:
+                messagebox.showerror("Error", "Password must be at least 8 characters long!")
+                return
+            else:
+                file = open("users.txt", "a", encoding="utf-8")
+                file.write(f"{username};{email};{password};user\n")
+                file.close()
+                messagebox.showinfo("Success", "User added successfully!")
+                newUsernameEntry.delete(0, "end")
+                newEmailEntry.delete(0, "end")
+                newPasswordEntry.delete(0, "end")
+                return
+
+def userLogout():
+    """
+    Função que faz logout do utilizador
+    Nesta função é atualizada a data do último logout do utilizador
+    caso o utilizador não tenha feito logout adiciona a data de logout quando
+    o mesmo fizer logout
+    """
+    listaUsers = lerFicheiroUsers()
+    for linha in listaUsers:
+        user = linha.split(";")
+        if user[0] == currentUser:
+            user[4] = str(datetime.datetime.now())
+            listaUsers.remove(linha)
+            file = open("users.txt", "w", encoding="utf-8")
+            file.writelines(listaUsers)
+            file.close()
+            messagebox.showinfo("Success", "Logged out successfully!")
+            app.destroy()
+            return
+            
+
+            
